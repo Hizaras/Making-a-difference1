21315ada63ca27642f1cb7a664541f8acb731d83
Hizaras <ihuezeblessing7@gmail.com>
2023-02-16 11:21:18 +0100

    bmi calculator

---
+++ src/BodyMassIndex.java
@@ -0,0 +1,26 @@
+
+//Making a Difference
+//        2.33 (Body Mass Index Calculator) We introduced the body mass index (BMI) calculator in
+//        Exercise 1.10. The formulas for calculating BMI are
+
+//BMI weightInPounds × 703
+//        heightInInches × heightInInches
+//        = ------------------------------------------------------------------------------------
+
+//Create a BMI calculator that reads the user’s weight in pounds and height in inches (or, if you prefer,
+//        the user’s weight in kilograms and height in meters), then calculates and displays the user’s
+//        body mass index. Also, display the BMI categories and their values from the National Heart Lung
+//        and Blood Institute
+//        http://www.nhlbi.nih.gov/health/educational/lose_wt/BMI/bmicalc.htm
+//        so the user can evaluate his/her BMI.
+//        [Note: In this chapter, you learned to use the int type to represent whole numbers. The BMI
+//        calculations when done with int values will both produce whole-number results. In Chapter 3
+//        you’ll learn to use the double type to represent numbers with decimal points. When the BMI calculations
+//        are performed with doubles, they’ll both produce numbers with decimal points—these
+//        are called “floating-point” numbers.]
+
+
+public class BodyMassIndex {
+
+
+}
